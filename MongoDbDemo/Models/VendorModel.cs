9f14247fecf4b63b2e37c7df23132eae989efa78
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-01-16 16:39:56 +0500

    Add sample model

---
+++ MongoDbDemo/Models/VendorModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDbDemo.Models
+{
+    public class VendorModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Info { get; set; }
+    }
+}
